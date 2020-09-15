d5724093214d8d353006ad4f2a6ad22a6db3f0b4
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2020-09-15 17:25:33 +0900

    sign up コンポーネント分割

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { MuiThemeProvider } from '@material-ui/core/styles'  // 追加
-import { theme } from '../../materialui/theme'  // 追加
-
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { theme } from '../../materialui/theme'
+import RecoInput from '../atoms/RecoInput'
+import RecoButton from '../atoms/RecoButton'
 
 
 function Copyright() {
@@ -62,7 +57,7 @@ export default function SignUp() {
                         variant="h5"
                         style={{ color: '#5f4339' }}
                     >
-                        Create Your Account
+                        Create Account
                     </Typography>
                     <form className={classes.form} noValidate>
                         <Grid
@@ -71,50 +66,28 @@ export default function SignUp() {
                             alignItems="center"
                         >
                             <Grid item xs={12}>
-                                <TextField
-                                    autoComplete="fname"
+                                <RecoInput
                                     name="userName"
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="userName"
                                     label="User Name"
-                                    autoFocus
                                 />
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
+                                <RecoInput
                                     name="email"
-                                    autoComplete="email"
+                                    label="Email Address"
                                 />
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="password"
+                                <RecoInput
+                                    name="Password"
                                     label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
                                 />
                             </Grid>
                             <Grid item xs={6} >
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary"
-                                    className={classes.submit}
-                                >
-                                    Create
-                                </Button>
+                                <RecoButton
+                                    className="classes.submit"
+                                    text="Create"
+                                />
                             </Grid>
                         </Grid>
                     </form>
